37216581b3da45e37028b0158b52ad801276c5c0
Santiago Guevara <saguevaraq@ufpso.edu.co>
2024-05-15 12:33:02 -0500

    Update 1

---
+++ src/components/vue/Directives.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'; //Anadir Reactividad
+    let msg = ref("Soy un tooltip");
+    let checked = ref(false);
+
+    let data = ["Hola", "Mundo", "Cruel"];
+//    let filtered = data.filter((word) => word.includes('o'));
+let filtered = data.filter(function (word) {
+    return word.includes('o')
+})
+</script>
+
+<template>
+    <input class="border"
+    :class="{'border-red-500': checked}"
+     v-model="msg"
+     /> <!-- v-model Habilitar doble vía -->
+    <div :title="msg"> <!--v-bind: -> : -->
+        {{ msg }}
+    </div>
+    <label for="check">seleccionado {{ checked }}</label>
+    <input type="checkbox" v-model="checked">
+    
+    <ul>
+        <li v-for="(text, index) in filtered" :key="index">
+            {{ text }}
+        </li>
+    </ul>
+</template>
---
+++ src/components/vue/TODO.vue
@@ -0,0 +1,26 @@
+<script setup>
+    import { ref } from 'vue';
+    const tasks = ref([]);
+    const inputTask = ref('');
+    function addTask(){
+        tasks.value.push(inputTask.value);
+        console.log(tasks);
+    }
+</script>
+
+<template>
+    <div class="max-w-96 bg-slate-300 rounded shadow mx-auto mt-10 py-8 px-10">
+        <h1 class="text-center text-lg">Lista de tareas</h1>
+        <div class="mt-4 mb-2">
+            <input v-model="inputTask" class="w-4/5 "/>
+            <button @click="addTask" class="w-1/5 bg-green-600 text-white font-bold">+</button>
+        </div>
+        <div 
+        v-for="(task,index) in tasks"
+        :key="index"
+        class="bg-white/60 px-2 mb-1">
+            {{ task }}
+        </div>
+
+    </div>
+</template>
